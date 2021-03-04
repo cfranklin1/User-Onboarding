b121613533696759369b0deaf722be4f0f367288
camille <spacejamilli20@gmail.com>
2021-03-04 14:52:44 -0600

    added some styling-components

--- an-app/src/App.js
+++ an-app/src/App.js
@@ -1,8 +1,18 @@
-import './App.css';
 import Form from './Form';
 import User from './User';
 import axios from 'axios';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import styled, {createGlobalStyle, css} from 'styled-components';
+
+const StyledHeader = styled.h1`
+ font-size: 1.2em;
+ text-decoration: underline;
+ display: flex;
+ justify-content: center;
+ margin: 0 auto;
+`
+
+
 
 const initialValues = {
   username: '',
@@ -16,7 +26,6 @@ const initialErrors = {
   password: '',
 }
 
-const initialDisabled = true
 
 
 
@@ -24,7 +33,6 @@ function App() {
   const [users, setUsers] = useState([])
   const [formValues, setFormValues] = useState(initialValues)
   const [formErrors, setFormErrors] = useState(initialErrors)
-  const [disabled, setDisabled] = useState(initialDisabled)
   
   
   const getUsers = () => {
@@ -61,6 +69,10 @@ function App() {
       email: formValues.email.trim(),
       password: formValues.password.trim(),
 
+    } 
+    
+    if (!newUser.username || !newUser.email || !newUser.password) {
+      return;
     }
     postNewUser(newUser);
     
@@ -70,17 +82,17 @@ function App() {
     getUsers()
   }, [])
 
-
+ 
 
   return (
     <div className="App">
-      <h1>An App</h1>
+      <StyledHeader><h1>An App</h1></StyledHeader>
+      
       <Form 
         users={users}
         values={formValues}
         submit={submitForm}
         change={changeInput}
-        disabled={disabled}
         errors={formErrors}
       />
 
--- an-app/src/Form.js
+++ an-app/src/Form.js
@@ -1,7 +1,112 @@
 import React from 'react'
+import styled, {createGlobalStyle, css} from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    html {
+        height: 100%;        
+        background: linear-gradient(to top, #000, #fff);
+
+    }
+    body {
+        font-family: monospace;
+        height: 100%;
+        margin: 0 auto;
+        color: #000;
+    }
+`;
+
+const sharedStyles = css`
+    background-color: #eee;
+    height: 4em;
+    border-radius: .5em;
+    border: 1px solid #D64161;
+    margin: 1em 0 2em 0;
+    padding: 1em;
+    box-sizing: border-box;
+`;
+
+const StyledFormWrapper = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+    padding: 0 ;
+`;
+
+const StyledForm = styled.form`
+    color: whitesmoke;
+    font-size: 1.2em;
+    width: 100%;
+    max-width: 70em;
+    padding: 4em;
+    background-color: #000;
+    border-radius: 1em;
+    box-sizing: border-box;
+    box-shadow: 0 0 10em 0 #fff;
+    margin: 0; 
+
+    h3{
+        display: flex;
+        justify-content: center;
+    }
+
+`;
+const StyledFieldset = styled.fieldset`
+    display: flex;
+    justify-content: center;
+    border: 1px solid #fff;
+    border-radius: .5em;
+    padding: 1em;
+    margin: 2em 0;
+    color: #fff;
+    :hover, :hover a {
+        border: 1px solid #D64161;
+        color: #fff;
+    }
+    label a{
+        color: blue;
+    }
+    
+    input {
+        
+        margin-left: 1em;
+    }
+`;
+const StyledInput = styled.input`
+    display: block;
+    width: 100%;
+    ${sharedStyles}
+    :hover {
+        border: .2em solid #FF7B25;
+    }
+`;
+
+const StyledButton = styled.button`
+    display: inline-block;
+    align-items: center;
+    background-color: #fff;
+    color: #D64161;
+    font-size: 1rem;
+    border: 0;
+    border-radius: .5em;
+    height: 2em;
+    padding: 0 2em;
+    cursor: pointer;
+    box-sizing: border-box;
+    
+    :hover {
+        background-color: #0A1172;
+        color: #fff;
+        font-weight: bold;
+        text-transform: uppercase; 
+    }
+`;
+
+
+
 
 export default function Form(props)   {
-    const { values, submit, change, disabled} = props    
+    const { values, submit, change} = props    
 
     const onSubmit = e => {
         e.preventDefault()
@@ -15,49 +120,57 @@ export default function Form(props)   {
     }
 
     return (
-        <form className='form-container' onSubmit={onSubmit}>
-           <h3>sign in</h3>
-            <div className='form-inputs'>
+       <>
+        <GlobalStyle />
+        <StyledFormWrapper>
+            <StyledForm onSubmit={onSubmit}>
+                <h3>sign in</h3>
+            
                 <label>Name
-                    <input name='username' 
+                    <StyledInput name='username' 
                         type='text' 
                         value={values.username} 
                         onChange={onChange} 
                         placeholder='your name..' 
                         maxLength='25'
                         />
-                </label><br/>
+                </label>
                 <label>Email
-                    <input name='email' 
+                    <StyledInput name='email' 
                         type='email' 
                         value={values.email} 
                         onChange={onChange} 
                         placeholder='name@email.com'
                         />
-                </label><br/>
+                </label>
                 <label>Password
-                    <input name='password' 
+                    <StyledInput name='password' 
                         type='password'
                         value={values.password} 
                         onChange={onChange}
                         placeholder='********'
                         minLength='8' 
                         /> 
-                </label><br/>
-                <label>Terms of Service
+                </label>
+                <StyledFieldset>
+                <label><a href='#'>Terms of Service</a>
                     <input name='termsofservice' 
                         type='checkbox' 
                         checked={values.termsofservice}
                         onChange={onChange} 
                         />
                 
-                </label><br/>
+                </label>
+                </StyledFieldset>
 
                 <label>
-                    <button type='submit'>submit</button>
+                    <StyledButton type='submit'>enter</StyledButton>
                 </label>
-            </div>
-        </form>
+                
+            
+            </StyledForm>
+            </StyledFormWrapper>
+        </>
     )
 
 }
